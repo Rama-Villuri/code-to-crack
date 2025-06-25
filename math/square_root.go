931c39c5d787e7f8c13210ef0d198fb6ab9b2d2f
Rama-Villuri <villurirama@gmail.com>
2025-06-25 07:09:15 +0530

    fixed lint reported issues

--- math/square_root.go
+++ math/square_root.go
@@ -1,8 +1,9 @@
-// File: math/square_root.go
+// Package math provides basic mathematical functions.
 package math
 
 import "math"
 
+// SquareRoot returns the square root of a given number x.
 func SquareRoot(x float64) float64 {
 	return math.Sqrt(x)
 }
