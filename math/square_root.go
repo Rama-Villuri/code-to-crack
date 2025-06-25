1f897e89881cdd9dac6750237d53c2a395ea8b4c
Rama-Villuri <villurirama@gmail.com>
2025-06-25 08:20:58 +0530

    Test branche:

--- math/square_root.go
+++ math/square_root.go
@@ -1,9 +1,13 @@
 // Package math provides basic mathematical functions.
 package math
 
-import "math"
+import (
+	"fmt"
+	"math"
+)
 
 // SquareRoot returns the square root of a given number x.
 func SquareRoot(x float64) float64 {
+	fmt.Println("Calculating square root of", x)
 	return math.Sqrt(x)
 }
